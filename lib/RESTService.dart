d6fc9759e7bd13c253480dbfe9fcfe9fa9ef5a8a
ozeeeno <raphael119@snu.ac.kr>
2023-06-02 09:34:57 +0900

    edit link

--- lib/RESTService.dart
+++ lib/RESTService.dart
@@ -8,7 +8,7 @@ import 'calculation_data.dart';
 class RESTService {
   static Future<Object> sendData(String illuminance, String area) async {
     final response = await http.get(Uri.parse(
-        'http://127.0.0.1:8000/calculation?illuminance=$illuminance&area=$area'));
+        'https://drone-api.vercel.app/calculation?illuminance=$illuminance&area=$area'));
     if (response.statusCode == 200) {
       return CalculationData.fromJson(
           jsonDecode(utf8.decode(response.bodyBytes)));
